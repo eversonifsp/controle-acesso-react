ef9693712dca9d080610d6b8f33bc2f8ff33ba17
LeleoLP <129314381+LeleoLP@users.noreply.github.com>
2023-11-16 13:17:27 -0300

    Atualization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CameraComponent from './components/Camera';
+
+const App = () => {
+  return (
+    <div>
+      <CameraComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Reader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReadQR from './components/ReadQR';
+
+function App() {
+  return (
+    <div className="App">
+      <ReadQR/>
+    </div>
+  );
+}
+
+export default App;
