e992090c39c6a496bf34a2fc9fe62f33129565e4
duduardo13 <eduardofontes430@gmail.com>
2023-11-20 13:01:54 -0300

    Acertos de Tags

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
     <div className="container">
       
         <form className="form-login">
-        
-          <fieldset className='fieldset-form-login'>
 
             <div className='line'>
               
@@ -34,10 +32,10 @@ function App() {
 
             </div>
 
-          </fieldset> 
-
         </form>
 
     </div>
   );
 }
+
+export default App;
