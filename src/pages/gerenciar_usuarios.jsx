e408a44b42c4a44405461ad340ad6eb239bbc16e
duduardo13 <eduardofontes430@gmail.com>
2023-12-02 17:10:53 -0300

    idealizando a tela de gerenciar users

--- src/pages/gerenciar_usuarios.jsx
+++ src/pages/gerenciar_usuarios.jsx
@@ -22,15 +22,58 @@ function Gerenciar() {
   };
 
   return (
-    <div className="container-gerenciar">
-
-    <div className="line-adm">
+    <fragment>
+<header>
+    <div className="line-gerenciar">
         <div className="col-gerenciar"> <h2>Gerenciar Usuários</h2> </div>
         <div className='col-gerenciar-logo'> <img src={logoif} alt="Logo do Instituto Federal de Cubatão" className='logoif'/> </div>
     </div>
+  </header>
 
-       
+
+    <main>
+
+    <div className="container-usuario">
+
+<div className="usuario">
+
+      <div className="line-usuario">
+          <div className="col-prontuario-usuario">
+              <p><strong>Protuário/CPF:</strong> CB000000X </p>
+          </div>
+      </div>
+
+      <div className="line-usuario">
+          <div className="col-name-usuario">
+              <p><strong>Nome:</strong> Ronaldo Gaúcho </p>
+          </div>
+        </div>
+
+      <div className="line-usuario">
+          <div className="col-telefone-usuario">
+              <p> <strong>Telefone:</strong> 13 000000000 </p>
+          </div>
+        </div>
+
+      <div className="line-usuario">
+          <div className="col-email-usuario">
+              <p> <strong>E-mail:</strong>  ifsp@email.com  </p>
+          </div>
+        </div>
+
+      <div className="line-usuario">
+          <div className="col-tipo-usuario">
+              <p> <strong>Tipo de Usuário:</strong> Portas </p>
+          </div>
+        </div>
+        
+    
     </div>
+</div>
+    </main>
+
+    <footer></footer>
+   </fragment>
   );
 }
 
