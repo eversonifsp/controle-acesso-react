50abd2c40d7eaebb8f2f6e5c4a032a3383c422f3
Sergio Wu <srwu00@gmail.com>
2023-12-02 19:53:52 -0300

    testing token at login and concierge

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,87 +1,97 @@
-import './css/login.css';
-import logoif from '../img/logoIF.png';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate, Link } from "react-router-dom";
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css'; 
-
+import "./css/login.css";
+import logoif from "../img/logoIF.png";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function Login() {
-        
   const [valor, setValor] = useState({
-    prontuario: '',
-    password: ''
+    prontuario: "",
+    password: "",
   });
   const history = useNavigate();
-  const valorEntrada = (e) => setValor({ ...valor, [e.target.name]: e.target.value});
+  const valorEntrada = (e) =>
+    setValor({ ...valor, [e.target.name]: e.target.value });
 
   const loginUs = async (e) => {
     e.preventDefault();
-    console.log(valor); // Verifique se os dados do input estão corretos no console
+    console.log(valor);
     try {
-      const response = await axios.post('http://localhost:3000/login', {
-        usuario: {  // Adapte para a estrutura esperada pelo seu backend
+      const response = await axios.post("http://localhost:3000/login", {
+        usuario: {
           prontuario: valor.prontuario,
-          password: valor.password
-        }
+          password: valor.password,
+        },
       });
 
       if (response.status === 200) {
-        console.log('Login bem-sucedido!');
-        // Redirecione para a rota desejada
-        history('/portaria')
+        // Captura o token no headers
+        const token = response.headers.authorization;
+        // Armazena no localStorage
+        localStorage.setItem("token", token);
+        console.log("Token:", token);
+        console.log("Login bem-sucedido!");
+
+        const storedToken = localStorage.getItem("token");
+          if (storedToken) {
+            history("/portaria");
+          }
       } else {
-        toast.error("Usuario ou senha invalida!")
-        // Adicione código para lidar com falha no login, se necessário
+        toast.error("Usuario ou senha invalida!");
       }
     } catch (error) {
-      toast.error("Usuario ou senha invalida!")
-      // Adicione código para lidar com erros, se necessário
+      toast.error("Usuario ou senha invalida!");
     }
   };
-  
-  
-  
-  
 
   return (
     <div className="container-login">
-      
-        <form className="form-login" onSubmit={loginUs}>
+      <form className="form-login" onSubmit={loginUs}>
+        <div className="line-login">
+          <div className="col-login"> Fazer Login </div>
 
-            <div className='line-login'>
-              
-                <div className='col-login'> Fazer Login </div>
+          <div className="col-login-logo">
+            {" "}
+            <img
+              src={logoif}
+              alt="Logo do Instituto Federal de Cubatão"
+              className="logoif"
+            />{" "}
+          </div>
+        </div>
 
-                <div className='col-login-logo'> <img src={logoif} alt="Logo do Instituto Federal de Cubatão" className='logoif'/> </div>
-            
-            </div>
+        <div className="col-user-login">
+          <input
+            type="text"
+            name="prontuario"
+            className="form-control-login"
+            placeholder="Usuário"
+            onChange={valorEntrada}
+          ></input>
+        </div>
 
-              <div className='col-user-login'>
-                <input type="text" name="prontuario"className="form-control-login" placeholder="Usuário" onChange={valorEntrada}></input>
-              </div>
+        <div className="col-pass-login">
+          <input
+            type="password"
+            name="password"
+            className="form-control-login"
+            placeholder="Senha"
+            onChange={valorEntrada}
+          ></input>
+        </div>
 
-              <div className='col-pass-login'>
-                <input type="password" name="password" className="form-control-login" placeholder="Senha" onChange={valorEntrada}></input>
-              </div>
-
-              <ToastContainer />
-              <div className='col-button-login'>
-                <button type='submit' className="button-login"> Entrar </button>
-
-              </div>
-
-
-        </form>
-
+        <ToastContainer />
+        <div className="col-button-login">
+          <button type="submit" className="button-login">
+            {" "}
+            Entrar{" "}
+          </button>
+        </div>
+      </form>
     </div>
-
-
   );
 }
 
-
 export default Login;
-
-
