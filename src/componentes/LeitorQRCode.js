e1b6b195fb718aedc16c414bd91f6f924710d731
Sergio Wu <srwu00@gmail.com>
2023-12-05 21:19:00 -0300

    camera ok

--- src/componentes/LeitorQRCode.js
+++ src/componentes/LeitorQRCode.js
@@ -1,58 +1,54 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import { useNavigate } from "react-router-dom";
 
-const Read = (props) => {
-  const [prontuario, setProntuario] = useState('No result');
+const LeitorQRCode = (props) => {
+  const [prontuario, setProntuario] = useState("No result");
+  const history = useNavigate();
+
+  const handleScan = (result, error) => {
+    if (!!result) {
+      alert("Prontuário: ${result}\nAluno liberado!");
+      setProntuario(result);
+    }
+
+    if (!!error) {
+      console.info(error);
+    }
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+  };
+
+  const closeCam = () => {
+    history("/entrada");
+    window.location.reload();
+  };
 
   return (
     <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            alert(`Prontuário: ${result.text}\nAluno liberado!`);
-            setProntuario(result.text);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        constraints={{
-          facingMode: 'environment'
-        }}
-        style={{ width: '100%' }}
-      />
+      <button
+        onClick={closeCam}
+        style={{
+          backgroundColor: "lightgreen",
+          borderRadius: "5px",
+          fontWeight: "bold",
+          fontSize: "1rem",
+          padding: ".5em",
+        }}>
+        Fechar Camera
+      </button>
+      {
+        <QrReader
+          onResult={handleScan}
+          onError={handleError}
+          constraints={{ facingMode: "environment" }}
+          style={{ width: "100%" }}
+        />
+      }
     </>
   );
 };
-
-
-const registraEntrada = async (e) => {
-  const prontuario = valor.prontuario;
-  e.preventDefault();
-
-  if (registrado == "nao_registrado") {
-  try {
-    console.log(storedToken);
-
-    const response = await apiClient.post(
-      `/registro_acesso_usuarios?prontuario=${valor.prontuario}`,
-      {
-        tipo: "entrada",
-      },
-      {
-        headers: { Authorization: storedToken },
-      }
-    );
-
-    console.log(valor.prontuario);
-    if (response.status === 201) {
-      toast.success("Entrada registrada com sucesso");
-    }
-  } catch (error) {
-    toast.error("Prontuario invalido!");
-  }
-}
-}
 
 export default LeitorQRCode;
