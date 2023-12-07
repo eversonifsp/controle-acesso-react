2426aa2ff79d58064ac98ff52cee45800f7db9e5
eversonclear <everson.pereira.clear@gmail.com>
2023-12-06 21:44:53 -0300

    feat: getting users

--- src/pages/gerenciar_usuarios.jsx
+++ src/pages/gerenciar_usuarios.jsx
@@ -1,138 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { IoArrowBackCircle } from "react-icons/io5";
-import logoif from "../img/logoIF.png";
 import apiClient from "../config/apiClient";
 import { useNavigate } from "react-router-dom";
 import "./css/gerenciar.css";
 
 function Gerenciar() {
   const [usuarios, setUsuarios] = useState([]);
-  const [novoUsuario, setNovoUsuario] = useState({
-    prontuario: "",
-    cpf: "",
-    nome: "",
-    telefone: "",
-    email: "",
-    tipo: "",
-    password: "",
-  });
   const history = useNavigate();
-  const [showForm, setShowForm] = useState(false);
 
-  /* Consume a api buscando usuários */
-  useEffect(() => {
-    // Carregar usuários da API ao montar o componente
-    fetchUsuarios();
-  }, []);
-
-  const fetchUsuarios = async () => {
-    try {
-      const response = await apiClient.get("/usuarios");
-      console.log("Dados da API:", response.data);
-      setUsuarios(response.data);
-    } catch (error) {
-      console.error("Erro ao buscar usuários:", error);
-    }
+  const fetchUsuarios = () => {
+    console.log('nfasnfnaio')
+    apiClient.get("/usuarios", {
+      headers: {
+        authorization: localStorage.getItem("token"),
+      },
+    }).then(({ data }) => {
+      setUsuarios(data);
+    }).catch((error) => console.log('error', error))
   };
 
-  /* Adiciona o usuário */
-  const handleAddUsuario = async () => {
-    try {
-      const storedToken = localStorage.getItem("token");
+  useEffect(() => fetchUsuarios(), []);
 
-      const response = await apiClient.post("/usuarios", novoUsuario, {
-        headers: { Authorization: storedToken },
-      });
-
-      if (response.status === 201) {
-        fetchUsuarios(storedToken);
-        setNovoUsuario({
-          prontuario: "",
-          cpf: "",
-          nome: "",
-          telefone: "",
-          email: "",
-          tipo: "",
-          password: "",
-        });
-      } else {
-        console.error("Erro ao adicionar usuário");
-      }
-    } catch (error) {
-    }
-  };
-
-
-  /* Lógica para editar usuário */
-  const handleEditUsuario = (index) => {
-  };
-
-  /* Lógica para deletar usuário */
-  const handleDeleteUsuario = (index) => {
-  };
-
-  /* Modal e tabela */
   return (
     <div>
       <main>
-        <div className="container-usuario">
-          <div className="criar-usuario">
-            <button
-              className="button-criar"
-              type="button"
-              onClick={() => setShowForm(!showForm)}
-            >
-              {showForm ? "Fechar Formulário" : "Criar Usuário"}
-            </button>
-            <form style={{ display: showForm ? "block" : "none" }}>
-              <label>Prontuário:</label>
-              <input
-                type="text"
-                value={novoUsuario.prontuario}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, prontuario: e.target.value })}
-              />
-
-              <label>CPF:</label>
-              <input
-                type="text"
-                value={novoUsuario.cpf}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, cpf: e.target.value })}
-              />
-
-              <label>Nome:</label>
-              <input
-                type="text"
-                value={novoUsuario.nome}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, nome: e.target.value })}
-              />
-
-              <label>Telefone:</label>
-              <input
-                type="tel"
-                value={novoUsuario.telefone}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, telefone: e.target.value })}
-              />
-
-              <label>Email:</label>
-              <input
-                type="email"
-                value={novoUsuario.email}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, email: e.target.value })}
-              />
-
-              <label>Tipo:</label>
-              <input
-                type="text"
-                value={novoUsuario.tipo}
-                onChange={(e) => setNovoUsuario({ ...novoUsuario, tipo: e.target.value })}
-              />
-              <button className="button-criar" type="button" onClick={handleAddUsuario}>
-                Criar Usuário
-              </button>
-            </form>
-          </div>
-        </div>
-
         <div className="tabela">
           <table>
             <thead>
@@ -140,6 +31,9 @@ function Gerenciar() {
                 <th>
                   {" "}
                   <h2>Prontuário</h2>{" "}
+                </th>
+                <th>
+                  <h2>CPF</h2>
                 </th>
                 <th>
                   <h2>Nome</h2>
@@ -160,29 +54,29 @@ function Gerenciar() {
             </thead>
 
             <tbody>
-              {usuarios.map((usuarios, index) => (
+              {usuarios.map((usuario, index) => (
                 <tr key={index}>
                   <td>
-                    <p>{usuarios.prontuario}</p>
+                    <p>{usuario.prontuario || '-'}</p>
                   </td>
                   <td>
-                    <p>{usuarios.cpf}</p>
+                    <p>{usuario.cpf}</p>
                   </td>
                   <td>
-                    <p>{usuarios.nome}</p>
+                    <p>{usuario.nome}</p>
                   </td>
                   <td>
-                    <p>{usuarios.telefone}</p>
+                    <p>{usuario.telefone || '-'}</p>
                   </td>
                   <td>
-                    <p>{usuarios.email}</p>
+                    <p>{usuario.email || '-'}</p>
                   </td>
                   <td>
-                    <p>{usuarios.tipo}</p>
+                    <p>{usuario.tipo}</p>
                   </td>
                   <td>
-                    <button onClick={() => handleEditUsuario(index)}>Editar</button>
-                    <button onClick={() => handleDeleteUsuario(index)}>Excluir</button>
+                    <button onClick={() => ({})}>Editar</button>
+                    <button onClick={() => ({})}>Excluir</button>
                   </td>
                 </tr>
               ))}
