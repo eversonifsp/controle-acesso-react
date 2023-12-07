ebfc31c652917b358a015d39cbea38d8570b384e
giovanneaquino <giovannebrandao13@gmail.com>
2023-12-07 01:42:33 -0300

    mais um erro

--- src/pages/entrada.jsx
+++ src/pages/entrada.jsx
@@ -29,7 +29,7 @@ function Entrar() {
 
   const registraEntrada = async (e) => {
     e.preventDefault();
-    console.log('valor', valor)
+    console.log("valor", valor);
 
     if (registrado == "nao_registrado") {
       try {
@@ -103,8 +103,9 @@ function Entrar() {
           ></input>
         </div>
 
-        {registrado === "registrado" && (
+        {registrado === "nao_registrado" && (
           <div className="campos-cadastrar" id="campos">
+            {/* Renderize os campos do formulário aqui */}
             <div className="col-name-entrar">
               <input
                 type="text"
@@ -137,66 +138,68 @@ function Entrar() {
           </div>
         )}
 
-          {/* radios button */}
+        {/* radios button */}
         <div className="line-radio">
-          <div className="line-line-radio">
-            <div className="col-nao-registrado">
-              <input
-                type="radio"
-                className="form-radio"
-                name="opcao"
-                id="opc1"
-                value="nao_registrado"
-                onChange={(e) => setRegistrado(e.target.value)}
-                checked={registrado === "nao_registrado"}
-              ></input>
-              <label for="opc1" className="reg">
-                {" "}
-               Registrado
-              </label>
-            </div>
-          </div>
-
           <div className="line-line-radio">
             <div className="col-registrado">
               <input
                 type="radio"
                 className="form-radio"
                 name="opcao"
-                id="opc2"
+                id="opc1"
                 value="registrado"
                 onChange={(e) => setRegistrado(e.target.value)}
                 checked={registrado === "registrado"}
               ></input>
+              <label for="opc1" className="reg">
+                {" "}
+                Registrado
+              </label>
+            </div>
+          </div>
+
+          <div className="line-line-radio">
+            <div className="col-nao-registrado">
+              <input
+                type="radio"
+                className="form-radio"
+                name="opcao"
+                id="opc2"
+                value="nao_registrado"
+                onChange={(e) => setRegistrado(e.target.value)}
+                checked={registrado === "nao_registrado"}
+              ></input>
               <label for="opc2" className="reg">
                 {" "}
-
                 Não Registrado{" "}
-
               </label>
             </div>
           </div>
         </div>
         <div className="col-camera-entrar" id="QRcode">
-          <button
-            className="button-camera-entrar"
-            type="button"
-            onClick={() => history("/FotoVisitante")}
-          >
-            {" "}
-            <FaCamera /> Foto visitante{" "}
-          </button>
+          {registrado === "nao_registrado" && (
+            <button
+              className="button-camera-entrar"
+              type="button"
+              onClick={() => history("/FotoVisitante")}
+            >
+              {" "}
+              <FaCamera /> Foto visitante{" "}
+            </button>
+          )}
         </div>
 
         <div className="col-camera-entrar" id="campos">
-          <button
-            className="button-camera-entrar"
-            type="button"
-            onClick={() => history("/LeitorEntry")}
-          >
-            {" "}
-            <IoQrCodeOutline /> Ler QRcode{" "}
-          </button>
+          {registrado === "registrado" && (
+            <button
+              className="button-camera-entrar"
+              type="button"
+              onClick={() => history("/LeitorEntry")}
+            >
+              {" "}
+              <IoQrCodeOutline /> Ler QRcode{" "}
+            </button>
+          )}
         </div>
 
         <div className="line-button-entrar">
